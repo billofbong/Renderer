cef4eb02f945612782f964e71f3365285d2dd19f
Will Wright <willjwright@mac.com>
2016-12-09 22:07:33 -0800

    Renders stuff now.

--- Renderer/src/org/billofbong/renderer/Renderer.java
+++ Renderer/src/org/billofbong/renderer/Renderer.java
@@ -2,11 +2,14 @@ package org.billofbong.renderer;
 
 import java.awt.BorderLayout;
 import java.awt.Canvas;
+import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.image.BufferStrategy;
+import java.awt.image.BufferedImage;
+import java.awt.image.DataBufferInt;
 
 import javax.swing.JFrame;
-
-import org.omg.CORBA.PRIVATE_MEMBER;
 
 public class Renderer extends Canvas implements Runnable {
 
@@ -14,7 +17,7 @@ public class Renderer extends Canvas implements Runnable {
 	private static final long serialVersionUID = 1L;
 	
 	public static final int WIDTH = 1080;
-	public static final int HEIGHT = WIDTH / 16 * 9;
+	public static final int HEIGHT = WIDTH / 12 * 9;
 	public static final int SCALE = 3;
 	
 	public static final String NAME = "Renderer";
@@ -22,6 +25,10 @@ public class Renderer extends Canvas implements Runnable {
 	private JFrame frame;
 	
 	public boolean running = false;
+	public int tickCount = 0;
+	
+	private BufferedImage image = new BufferedImage(WIDTH, HEIGHT, BufferedImage.TYPE_INT_RGB);
+	private int[] pixels = ((DataBufferInt) image.getRaster().getDataBuffer()).getData();
 	
 	public Renderer()
 	{
@@ -53,26 +60,39 @@ public class Renderer extends Canvas implements Runnable {
 		
 		while(running)
 		{
-			System.out.println("Running");
 			long now = System.nanoTime();
 			delta += (now - lastTime) / nsPerTick;
 			lastTime = now;
+			boolean shouldRender = false;
 			
 			while(delta >= 1)
 			{
 				ticks++;
 				tick();
 				delta -= 1;
+				shouldRender = true;
 			}
 			
-			render();
+			if(shouldRender)
+			{
+				frames++;
+				render();
+			}
+			
+			if(System.currentTimeMillis() - lastTimer >= (1000 * 0.416666))
+			{
+				lastTimer += 1000;
+				System.out.println("Frames: " + frames + ", " + "Ticks: " + ticks);
+				frames = 0;
+				ticks = 0;
+			}
 		}
 	}
 	
 	public synchronized void start()
 	{
 		running = true;
-		new Thread(this);
+		new Thread(this).start();
 	}
 	
 	public void stop()
@@ -82,12 +102,29 @@ public class Renderer extends Canvas implements Runnable {
 	
 	public void tick()
 	{
+		tickCount++;
 		
+		for(int i = 0; i < pixels.length; i++)
+		{
+			pixels[i] = i + tickCount;
+		}
 	}
 	
 	public void render()
 	{
+		BufferStrategy bufferStrategy = getBufferStrategy();
+		if(bufferStrategy == null)
+		{
+			createBufferStrategy(3);
+			return;
+		}
 		
+		Graphics graphics = bufferStrategy.getDrawGraphics();
+		graphics.setColor(Color.BLACK);
+		graphics.drawRect(0, 0, getWidth(), getHeight());
+		graphics.drawImage(image, 0, 0, getWidth(), getHeight(), null);
+		graphics.dispose();
+		bufferStrategy.show();
 	}
 
 	public static void main(String[] args)
